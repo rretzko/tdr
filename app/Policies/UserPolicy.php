8149da6f1242de38386a049f9085037a1d5a2ed5
MFRHoldings <rick@mfrholdings.com>
2023-02-03 16:19:55 -0500

    Added internal changes for enabling S3 storage as described in https://docs.vapor.build/1.0/resources/storage.html#file-uploads.

---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Policies;
+
+use Illuminate\Auth\Access\HandlesAuthorization;
+use App\Models\User;
+
+class UserPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\User  $model
+     * @return mixed
+     */
+    public function view(User $user, User $model)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  \App\Models\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\User  $model
+     * @return mixed
+     */
+    public function update(User $user, User $model)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\User  $model
+     * @return mixed
+     */
+    public function delete(User $user, User $model)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\User  $model
+     * @return mixed
+     */
+    public function restore(User $user, User $model)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\User  $model
+     * @return mixed
+     */
+    public function forceDelete(User $user, User $model)
+    {
+        //
+    }
+
+    /**
+     * @since 03-Feb-2023
+     * @url https://docs.vapor.build/1.0/resources/storage.html#file-uploads
+     *
+     * Determine whether the user can upload files.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function uploadFiles(User $user)
+    {
+        return true;
+    }
+}
